b9d5d8a3d79bd0ca214a933e11372626449433aa
zokrezyl <zokrezyl@gmail.com>
2025-12-21 11:23:27 +0100

    moving data objects as locals to the widget

--- docs/demo/js/pyodide_loader.js
+++ docs/demo/js/pyodide_loader.js
@@ -1,7 +1,7 @@
 // js/pyodide.js
 
 // Ymery version - updated by build process
-const YMERY_VERSION = "0.0.70";
+const YMERY_VERSION = "0.0.71";
 
 async function load_pyodide_imgui_render() {
     console.log('Loading load_pyodide_imgui_render.py');
